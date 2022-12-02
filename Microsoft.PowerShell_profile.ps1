90628badbebe54bee6e26dba5ff1867221bf3a22
Walnuts <r.juglans.1018@gmail.com>
2022-12-02 10:36:23 +0900

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+#Set-Alias nano "C:\Program Files(user)\nano"
+Set-Alias kubectl "C:\Program Files(user)\kubectl"
+Set-Alias iperf3 "C:\Program Files(user)\iperf-3.1.3-win64\iperf3.exe"
+#Set-Alias vim "nvim"
+
+Import-Module posh-git
+oh-my-posh init pwsh --config ~/.pwsh10k.omp.json | Invoke-Expression
