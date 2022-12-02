7a8a56a6fb2c09b902ec22366d4491f3deaa173b
Walnuts <r.juglans.1018@gmail.com>
2022-12-02 14:23:07 +0900

    Update Microsoft.PowerShell_profile.ps1

--- Documents/Powershell/Microsoft.PowerShell_profile.ps1
+++ Documents/Powershell/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,21 @@
-#Set-Alias nano "C:\Program Files(user)\nano"
+Set-Alias nano "C:\Program Files(user)\nano"
+
 Set-Alias kubectl "C:\Program Files(user)\kubectl"
+
 Set-Alias iperf3 "C:\Program Files(user)\iperf-3.1.3-win64\iperf3.exe"
+
 #Set-Alias vim "nvim"
 
 Import-Module posh-git
 oh-my-posh init pwsh --config ~/.pwsh10k.omp.json | Invoke-Expression
+
+function Invoke-As-Admin() {
+    if ($args.count -eq 0) {
+        gsudo
+        return
+    }
+    $cmd = $args -join ' '
+    gsudo "pwsh.exe -Login -Command { $cmd }"
+}
+
+Set-Alias -Name: "sudo" -Value: "Invoke-As-Admin"
