4419cf3799b6285edd49c3ed2fd5e0df66a8a457
Walnuts <r.juglans.1018@gmail.com>
2024-01-02 11:14:08 +0900

    add eza & ugrep

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,17 +1,21 @@
-$ErrorActionPreference = "SilentlyContinue"
 $profile_dir = Split-Path $profile
 
 Import-Module posh-git
 oh-my-posh init pwsh --config $profile_dir/.pwsh10k.omp.json | Invoke-Expression
 
-
 Set-PSReadLineOption -PredictionSource History -PredictionViewStyle ListView
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 
 Set-Alias -Name: "open" -Value: "explorer"
-Set-Alias -Name: "grep" -Value: "Select-String"
 Set-Alias -Name: "pbcopy" -Value: "Set-Clipboard"
 
-Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+# ugrep存在確認
+if (Get-Command ugrep -ea SilentlyContinue) {
+    Set-Alias -Name: "grep" -Value: "ugrep"
+}
+else {                                
+    Set-Alias -Name: "grep" -Value: "Select-String"
+}
 
 Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
     param($wordToComplete, $commandAst, $cursorPosition)
@@ -23,75 +27,45 @@ Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
     }
 }
 
-
-Set-Alias -Name: k -Value: kubectl
-Register-ArgumentCompleter -CommandName 'k' -ScriptBlock $__kubectlCompleterBlock
-
 kubectl completion powershell | Out-String | Invoke-Expression
 flux completion powershell | Out-String | Invoke-Expression
 helm completion powershell | Out-String | Invoke-Expression
 
-Remove-Alias -Name: "ls"
+Set-Alias -Name: k -Value: kubectl
+Register-ArgumentCompleter -CommandName 'k' -ScriptBlock $__kubectlCompleterBlock
 
-function ls {
-    $paths = @()
 
-    $rawoptions = @()
-    $options = @()
-    
-    $prefix = @()
-    $saffix = @()
-
-    foreach ($a in $args) {
-        if ($a.StartsWith("-") -And !($a.StartsWith("--"))) {
-            $tmp = $a.ToCharArray()
-            $rawoptions += $tmp[0..($tmp.length - 1)]
-        }
-        elseif ($a.StartsWith("--")) {
-            #TODO
-        }
-        else {
-            $paths += $a
-        }
-    }
-
-    $rawoptions = $rawoptions | Select-Object -Unique
-    foreach ($o in $rawoptions) {
-        switch ($o) {
-            #show hidden files
-            "a" { $options += "-Force" }
-        }
-    }
-
-    $prefix += '$result = New-Object -TypeName PSObject -Property @{Mode = "Mode"; LastWriteTime = "LastWriteTime"; Length = "Length"; Name = "Name"; LinkTarget = "LinkTarget"};'
-
-    #show hidden files & dot files
-    if ($rawoptions -contains "a") {}
-    else {
-        $saffix += '| Where-Object { $_.Name -NotMatch "^\." }'
-    }
-
-    #sort by size
-    if ($rawoptions -contains "S") {
-        $saffix += "| Sort-Object -Property Length -Descending"
-    }
-
-    #show file size in KB
-    if ($rawoptions -contains "h") {
-        $saffix += '|ForEach-Object {$result.Mode = $_.Mode; $result.LastWriteTime = $_.LastWriteTime; $result.Length = [string]([Math]::Round($_.Length/1024, 1, [MidpointRounding]::AwayFromZero))+"K"; $result.Name = $_.Name; $result.LinkTarget=$_.LinkTarget; $result}'
-    }
-
-    #show long format
-    if ($rawoptions -contains "l") {
-        $saffix += '| ForEach-Object {$result.Mode = $_.Mode; $result.LastWriteTime = $_.LastWriteTime; $result.Length = $_.Length; $result.Name = [string]($_.Name)+($null -ne $_.LinkTarget?" -> "+[string]($_.LinkTarget):""); $result} | Format-Table Mode,LastWriteTime,Length,Name'
-    }
-    else {
-        $saffix += "| Select-Object Name | Format-Wide -AutoSize"
-    }
-
-    $command = "$($prefix -join " ") Get-ChildItem $($options -join " ") $($paths -join " ") $($saffix -join " ")"
-    Write-Verbose $command
-    $command | Invoke-Expression
+# ------------------- eza -------------------
+# eza存在確認
+if (Get-Command eza -ea SilentlyContinue) {
+    Remove-Alias -Name: "ls"
 }
 
-$ErrorActionPreference = "Continue"
+function ls() {
+    eza --git -@ -g -mU --icons --time-style=long-iso --color=automatic --group-directories-first $args
+}
+
+function ll() {
+    ls -lh # Long format, git status
+}
+function l() {
+    ll -a # Long format, all files
+}
+function lr() {
+    ls -Tlh # Long format, recursive as a tree
+}
+function lx() {
+    ll -sextension # Long format, sort by extension
+}
+function lk() {
+    ll -ssize # Long format, largest file size last
+}
+function lt() {
+    ll -smodified # Long format, newest modification time last
+}
+function lc() {
+    ll -schanged # Long format, newest status change (ctime) last
+}
+function tree() {
+    ls -Tl --no-permissions --no-filesize --no-user --no-time
+}
